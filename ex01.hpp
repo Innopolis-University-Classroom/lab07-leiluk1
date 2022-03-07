241ef95ed5ace9c814a85b50cc5779568ab9228e
leiluk1 <88292173+leiluk1@users.noreply.github.com>
2022-03-07 14:29:02 +0300

    Update ex01.hpp

--- ex01.hpp
+++ ex01.hpp
@@ -2,10 +2,10 @@
 //change the below code
 // the function below now only works with integers, change it to work with any type
 // also fill out the body
-template <class T>
-void multiples (T& sum, int x, int n)
+template <typename T>
+void multiples (T& sum, T x, int n)
 {
-  sum = 1;
+  sum += 1;
     for (int i = 1; i <= n; i++) {
         sum += i * x;
     }
